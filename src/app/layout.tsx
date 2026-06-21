24ae3d2c49562d7692f01daae7873b39653829f8
ValeDev <valentecreativo@proton.me>
2026-06-21 04:07:39 -0600

    favicon updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,6 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#1e293b" />
-        <link rel="icon" href="/favicon.png" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
       </head>
